5b024a415a58fea9499b3a59527c9270a5243ae7
Calco Dev <calcopoddev@gmail.com>
2025-02-02 14:38:00 +0200

    linking to raylib via c build thing

--- build.zig
+++ build.zig
@@ -4,28 +4,21 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const raylib_dep = b.dependency("raylib-zig", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    const raylib = raylib_dep.module("raylib");
-    // const raygui = raylib_dep.module("raygui");
-    // const rlgl = raylib_dep.module("rlgl");
-    const raylib_artifact = raylib_dep.artifact("raylib");
-
     const exe = b.addExecutable(.{
         .name = "zone",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.linkLibrary(raylib_artifact);
-    exe.root_module.addImport("raylib", raylib);
-    // exe.root_module.addImport("rlgl", rlgl);
 
     b.installArtifact(exe);
 
+    const raylib_dep = b.dependency("raylib", .{ .target = target, .optimize = optimize });
+    const raylib = raylib_dep.artifact("raylib");
+    exe.linkLibrary(raylib);
+
     const run_cmd = b.addRunArtifact(exe);
+
     run_cmd.step.dependOn(b.getInstallStep());
 
     if (b.args) |args| {
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const rl = @import("raylib");
+const rl = @cImport(@cInclude("raylib.h"));
 
 const calc = @import("calc.zig");
 const gameState = @import("game.zig");
@@ -9,22 +9,22 @@ const entities = @import("entities.zig");
 var game: gameState.State = undefined;
 
 pub fn main() !void {
-    rl.initWindow(gameState.winWidth, gameState.winHeight, gameState.winTitle);
-    rl.setTargetFPS(gameState.gameFps);
+    rl.InitWindow(gameState.winWidth, gameState.winHeight, gameState.winTitle);
+    rl.SetTargetFPS(gameState.gameFps);
     // rl.setExitKey(rl.KeyboardKey.null);
 
     // const shader = rl.loadShader("res/shaders/default.vert", "res/shaders/default.frag") catch unreachable;
 
     // game = try gameState.State.init();
     // game.sceneIntro();
-    while (!rl.windowShouldClose()) {
-        rl.beginDrawing();
-        rl.clearBackground(rl.Color.black);
-        rl.endDrawing();
+    while (!rl.WindowShouldClose()) {
+        rl.BeginDrawing();
+        rl.ClearBackground(rl.BLACK);
+        rl.EndDrawing();
         // try game.tick();
         // try game.draw();
     }
     // game.deinit();
 
-    rl.closeWindow();
+    rl.CloseWindow();
 }

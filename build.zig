fa843d479b27c3a422b1c8c5f7b97d96fa182fbf
Calco Dev <calcopoddev@gmail.com>
2025-02-06 18:06:09 +0200

    set arch to x86_64, fixes some crc32 issues on intel?

--- build.zig
+++ build.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{
+        .default_target = .{
+            .cpu_arch = .x86_64,
+        },
+    });
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
